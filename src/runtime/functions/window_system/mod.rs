16abb856b162eaf8225ce2929d10577636cd895e
Junkui Zhang <364772080@qq.com>
2023-07-21 21:54:54 +0800

    dont show maximize button

--- src/runtime/functions/window_system/mod.rs
+++ src/runtime/functions/window_system/mod.rs
@@ -5,8 +5,11 @@ pub struct WindowManager {
 impl WindowManager {
     pub fn new(event_loop: &winit::event_loop::EventLoop<()>) -> Self {
         let window = winit::window::WindowBuilder::new()
-            .with_title("Hello, world!")
+            .with_title("Ruccolo Engine")
             .with_inner_size(winit::dpi::PhysicalSize::new(1280, 720))
+            .with_enabled_buttons(
+                winit::window::WindowButtons::CLOSE | winit::window::WindowButtons::MINIMIZE,
+            )
             .with_resizable(false)
             // .with_fullscreen(Some(winit::window::Fullscreen::Borderless(None)))
             .build(event_loop)
